72df14e8873c986394efff53f43e7b4edd8dce12
tuan2bee <tuan2bee@gmail.com>
2022-07-08 20:47:56 +0700

    ok

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -21,6 +21,11 @@ export default function handler(
 ) {
   req.url = req.url?.replace("api", "");
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Requested-With"
+  );
   return new Promise((resolve) => {
     // convert cookies to header Authorization
     const cookies = new Cookies(req, res);
